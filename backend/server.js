cdebab6d95cfdfa5609ef2a8237e9c55b77ee4e9
saud-charity <saud-charity@gmail.com>
2025-09-08 20:12:17 +0400

    🚀 12d version

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/pdfs", express.static(path.join(__dirname, "../public/pdfs")));
 app.use("/js", express.static(path.join(__dirname, "../public/js")));
-app.use("/pdfjs", express.static(path.join(__dirname, "../public/pdfjs")));
 
 // -----------------------------
 // ✅ Home page
