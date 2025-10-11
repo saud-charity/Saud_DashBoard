a6b195e2a976b68e3a298b57eb744409ce5621ce
saud_board <saud.charity.1@gmail.com>
2025-10-11 12:37:01 +0400

    Report File updating

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,19 @@
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("v1").then(cache => {
+    caches.open('report-cache-v1').then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/js/main.js",
-        "/css/style.css"
+        '/',
+        '/report.html',
+        '/css/main.css',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
